9d02028d74fdd69ad82f02fe6f69a00d44d8c655
Muhammad Qureshi <mqureshi@HW13381.local>
2017-10-17 20:44:38 -0500

    Push project to git

---
+++ kafkastorm/src/main/java/com/imad/streamlio/App.java
@@ -0,0 +1,15 @@
+package com.imad.streamlio;
+
+import org.apache.storm.trident.TridentTopology;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	TridentTopology object = new TridentTopology();
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
